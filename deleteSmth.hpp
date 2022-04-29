df1849894f666f1f8215a46771af7a76004bdb63
NeFlex2 <73160774+NeFlex2@users.noreply.github.com>
2022-04-29 11:34:31 +0300

    Delete deleteSmth.hpp

--- deleteSmth.hpp
+++
@@ -1,6 +0,0 @@
-<?php
-$mysql = new mysqli('localhost', 'root', 'root', 'список фильмов на завтра');
-$id = $_GET['id'];
-mysqli_query($mysql, "DELETE FROM `кинотеатр` WHERE `кинотеатр`.`id` = '$id'");
-header('Location:auth.php');
-?>
